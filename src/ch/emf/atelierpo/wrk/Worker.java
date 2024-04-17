cef70334d1f113b5868c6371d40055bcd43d5795
RibeiroJ01 <RibeiroJ01@WSTEMFA44-15.stu.net.fr.ch>
2024-04-17 09:13:07 +0200

    Correction

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -5,8 +5,16 @@ package ch.emf.atelierpo.wrk;
  * @author Mario Ramalho(EMF - Fribourg)
  */
 public class Worker {
-
-   
+    
+   /**
+    * Contrôle la plaque d'immatriculation de voiture en Suisse et la valide. Cette méthode doit être capable d'utiliser tous les cantons.
+    * 
+    * @param canton         le canton associé à la plaque (caractères valable : A-Z, seulement l'initale est valable "FR" pour "Fribourg")
+    * @param numeroDePlaque Le numéro associé à la plaque (caractères valable : 1-9 , limite de 10 caractère)
+    * 
+    * @return true si la plaque d'immatriculation est validé ou false si elle n'est pas validé
+    */
+     
     public boolean controlePlaqueImmatriculation(String canton, String numeroDePlaque) {
         return false;
     }
